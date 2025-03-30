16b94c057f5b9a660420445a5eaf716546d45141
Kavarera <rafli.030715@gmail.com>
2025-03-31 03:20:12 +0700

    first innit

---
+++ README.md
@@ -0,0 +1,3 @@
+# stokin_bloc
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/stokin_bloc/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.stokin_bloc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
